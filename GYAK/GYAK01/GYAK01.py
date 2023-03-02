7604b664d6ddeac3afef54df11d3031c3bac8c40
Shiraizawa <dorazoltan010416@gmail.com>
2023-03-02 20:45:46 +0100

    element_wise_sum function done

--- GYAK/GYAK01/GYAK01.py
+++ GYAK/GYAK01/GYAK01.py
@@ -13,3 +13,10 @@ def is_odd(input_list:list) ->bool:
             bool_list.append(False)
     return bool_list
 
+def element_wise_sum(input_list_1:list,input_list_2:list)->list:
+    new_list=[]
+    for x in range(0,len(input_list_1),1):
+        for y in range(0,len(input_list_2),1):
+            new_list.append(input_list_1[x]+input_list_2[y])
+    return new_list
+    
