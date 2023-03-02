702166d27be342e7b7d1310e8fe691764de9235d
Shiraizawa <dorazoltan010416@gmail.com>
2023-03-02 20:41:31 +0100

    is_odd function done

--- GYAK/GYAK01/GYAK01.py
+++ GYAK/GYAK01/GYAK01.py
@@ -3,3 +3,13 @@ def contains_odd(input_list:list)->bool:
         if (x%2)==1:
             return True
     return False
+
+def is_odd(input_list:list) ->bool:
+    bool_list=[]
+    for x in input_list:
+        if (x%2)==1:
+            bool_list.append(True)
+        else:
+            bool_list.append(False)
+    return bool_list
+
