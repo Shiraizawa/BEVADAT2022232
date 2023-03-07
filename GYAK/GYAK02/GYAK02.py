c335b82116feb5aa2e5b6ccfbc43b2e4103beff0
Shiraizawa <dorazoltan010416@gmail.com>
2023-03-07 12:40:03 +0100

    bool_array function created

--- GYAK/GYAK02/GYAK02.py
+++ GYAK/GYAK02/GYAK02.py
@@ -36,3 +36,13 @@ def do_transpose(matrix):
 def round_array(array, n:int=2):
      array=np.round(array,n)
      return array
+
+# Készíts egy olyan függvényt, ami a bementként  0 és 1 ből álló tömben a 0 - False-ra az 1 True-ra cserélni
+# Be: [[1, 0, 0], [1, 1, 1],[0, 0, 0]]
+# Ki: [[ True False False], [ True  True  True], [False False False]]
+# bool_array()
+
+def bool_array(array):
+     array=np.array(array, dtype=bool)
+     return array
+
