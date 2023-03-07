ed67f995924d445ea69ad668b08339df58bdae03
Shiraizawa <dorazoltan010416@gmail.com>
2023-03-07 12:53:53 +0100

    column_swap function created

--- HAZI/HAZI02/HAZI02.py
+++ HAZI/HAZI02/HAZI02.py
@@ -1,2 +1,10 @@
 import numpy as np
 
+# Írj egy olyan fügvényt, ami megfordítja egy 2d array oszlopait
+# Be: [[1,2],[3,4]]
+# Ki: [[2,1],[4,3]]
+# column_swap()
+
+def column_swap(array):
+    array=np.flip(array, 1)
+    return array
